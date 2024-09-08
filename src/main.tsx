0c6674b2b2092cb2ec4e330469e8c4fb2851bd1d
KooWeso <koowa360@gmail.com>
2024-09-08 22:38:58 +0300

    t

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ import './index.css'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <Router basename="/">
+      <Router>
         <Routes>
           <Route path="/*" element={<App />} />
         </Routes>
