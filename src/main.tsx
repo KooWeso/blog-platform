b4de15b86746c37ee18e6d85ee084b1fdc9e31b5
KooWeso <koowa360@gmail.com>
2024-09-08 21:55:04 +0300

    try

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ import './index.css'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <Router>
+      <Router basename="/">
         <Routes>
           <Route path="/*" element={<App />} />
         </Routes>
